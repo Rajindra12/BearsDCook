65a0cd21eb3d847087fbe22bed74681cf0fdf8c8
Rajindra12 <jindra.aziz@gmail.com>
2024-02-15 06:40:45 +0700

    ok

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+            // Check If form submitted, insert form data into users table.
+if(isset($_POST['Submit'])) {
+    $namas= $_POST['nama']; 
+    $usernames = $_POST['username']; 
+    $passwords = $_POST['password']; 
+    $levels= $_POST['level']; 
+    //echo($judul);
+    // include database connection file
+    include_once("koneksi.php");
+    
+    // Insert user data into table
+    $result = mysqli_query($mysqli,
+    "INSERT INTO user(nama, username, password, level) VALUES ('$namas', '$usernames', '$passwords', '$levels')");
+    
+    // Show message when user added
+    // echo "Data added successfully. <a href='index.php'>View Data Buku</a>";
+    header("location: index.php");
+}
+?>
